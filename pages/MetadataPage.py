f4b1281899f6f8e6cb272ad7ed3cbb515f05193c
Vincentius Adrian <wsamudro@gmail.com>
2024-06-01 18:49:46 +0700

    feat: change file name to include details and add summary

--- pages/MetadataPage.py
+++ pages/MetadataPage.py
@@ -4,6 +4,8 @@ from imageio_ffmpeg import get_ffmpeg_exe
 import subprocess
 import os
 from pathlib import Path
+import pprint
+import json
 
 TOP_PATH = Path(__file__).resolve().parent.parent.absolute()
 FFPROBE_PATH = os.path.join(TOP_PATH, "binaries", "ffprobe-win32-v4.1.exe")
@@ -52,7 +54,7 @@ class MetadataPage(tk.Frame):
             command=lambda: self.process_and_dump_details(),
         )
         self.run_trigger.place(
-            x=200,
+            x=400,
             y=200,
             width=178,
             height=43,
@@ -76,26 +78,6 @@ class MetadataPage(tk.Frame):
             os.mkdir(output_dir)
 
         try:
-            for frame_type in ("I", "P", "B"):
-                frames_dir = f"{output_dir}/{frame_type}_frame"
-                if not os.path.exists(frames_dir):
-                    os.mkdir(frames_dir)
-
-                cmd = [
-                    get_ffmpeg_exe(),
-                    "-i",
-                    self.video_path,
-                    "-vf",
-                    f"select=eq(pict_type\\,{frame_type})",
-                    "-vsync",
-                    "vfr",
-                    f"{frames_dir}/frame_%5d.jpg",
-                ]
-
-                p = subprocess.run(
-                    args=cmd,
-                )
-                
             all_frames_with_mvs_dir = f"{output_dir}/mvs"
             if not os.path.exists(all_frames_with_mvs_dir):
                 os.mkdir(all_frames_with_mvs_dir)
@@ -109,10 +91,11 @@ class MetadataPage(tk.Frame):
                     self.video_path,
                     "-vf",
                     "codecview=mv=pf+bf+bb",
-                    f"{all_frames_with_mvs_dir}/frame_%5d.jpg",
+                    f"{all_frames_with_mvs_dir}/frame_%5d.bmp",
                 ],
             )
 
+            res = None
             with open(f"{output_dir}/metadata.json", "w") as metadata_file:
                 p = subprocess.run(
                     args=[
@@ -124,6 +107,45 @@ class MetadataPage(tk.Frame):
                     ],
                     stdout=metadata_file,
                 )
+
+            with open(f"{output_dir}/metadata.json", "r") as metadata_file:
+                res = json.load(metadata_file)
+
+            if not res.get("frames") or len(res.get("frames")) <= 0:
+                return
+            
+            for idx, info in enumerate(res.get("frames")):
+                old_filename = f"{all_frames_with_mvs_dir}/frame_{idx+1:05d}.bmp"
+                if not os.path.exists(old_filename):
+                    messagebox.showinfo("Info", f"{old_filename} not found. Continuing ...")
+                    continue
+
+                os.replace(
+                    old_filename,
+                    f"{all_frames_with_mvs_dir}/frame_{idx+1:05d}_{info.get('pict_type')}_{info.get('pkt_size')}.bmp"
+                ) 
+
+            summary = {
+                "Overall": {
+                    "Total number of frame": len(res.get("frames")),
+                    "Total packet size in bytes": sum(map(lambda i: int(i.get("pkt_size")), res.get("frames"))),
+                },
+                "Total unrecognized frame type": sum(map(lambda i: int(i.get("pict_type") not in ("I", "P", "B")), res.get("frames"))),
+            }
+            types = set(map(lambda i: i.get("pict_type"), res.get("frames")))
+            for typ in types:
+                temp = list(filter(lambda i: int(i.get("pict_type") == typ), res.get("frames")))
+                packet_sizes = list(map(lambda i: int(i.get("pkt_size")), temp))
+                summary.update({
+                    f"{typ}-frame": {
+                        "Number of frame": len(temp),
+                        "Max size in byte": max(packet_sizes),
+                        "Min size in byte": min(packet_sizes),
+                        "Avg size in byte": sum(packet_sizes) / len(temp),
+                    }
+                })
+
+            pprint.pprint(summary)
 
         except Exception as e:
             messagebox.showerror("Error", e)
